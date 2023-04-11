9a34a36aadaaf7c038cc7fff8744da6f88f1332e
yunhal <yunha.lee.00@gmail.com>
2023-04-11 19:51:23 -0400

    remove unused test file

--- src/napari_hdf5_netcdf_viewer/_tests/test_quick_hdf5_netcdf_viewer.py
+++ src/napari_hdf5_netcdf_viewer/_tests/test_quick_hdf5_netcdf_viewer.py
@@ -1,7 +1,8 @@
+import napari
 from napari_hdf5_netcdf_viewer import make_widget
 
 
-def test_hdf5_netcdf_viewer(fpath: str):
+def test_hdf5_netcdf_viewer():
     """
     test to read file structure and open the images with napari if the path file format is hdf or netcdf.
 
