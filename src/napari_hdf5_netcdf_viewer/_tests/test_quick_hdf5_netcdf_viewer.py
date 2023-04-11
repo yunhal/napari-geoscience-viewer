146f3e5561039d5eb246569aacbf41231fdf9343
yunhal <yunha.lee.00@gmail.com>
2023-04-11 13:23:12 -0400

    reformatting

---
+++ src/napari_hdf5_netcdf_viewer/_tests/test_quick_hdf5_netcdf_viewer.py
@@ -0,0 +1,18 @@
+from napari_hdf5_netcdf_viewer import make_widget
+
+
+def test_hdf5_netcdf_viewer(fpath: str):
+    """
+    test to read file structure and open the images with napari if the path file format is hdf or netcdf.
+
+    Parameters
+    ----------
+    fpath: str
+        hdf5 or netcdf file path
+    -------
+    """
+
+    viewer = napari.Viewer()
+    viewer.window.add_dock_widget(make_widget(), area="right")  
+
+
